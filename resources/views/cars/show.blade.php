1c74be8c11a54f99377359bb1e58031bd9048bca
Goran Jovicic <gugacns94@gmail.com>
2019-07-24 12:30:30 +0200

    zadatak 7 home

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cars</title>
+    <title>Single car</title>
 </head>
 <body>
-    @foreach ($cars as $car)
-        <div>
-            <p><a href="/cars/{{ $car->id }}">{{ $car->title }}</a></p>
-        </div>
-    @endforeach
+    <div>
+        <p> {{ $car->title }} </p>
+        <p> {{ $car->producer }} </p>
+        <p> {{ $car->number_of_doors }} </p>
+    </div>
 </body>
 </html>
