7761740dfe35a90c415c9f7f85a6652dccb83bb7
Goran Jovicic <gugacns94@gmail.com>
2019-07-24 12:19:41 +0200

    zadatak 4 i 5 hoem

--- database/migrations/2019_07_24_101644_create_cars_table.php
+++ database/migrations/2019_07_24_101644_create_cars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            Schema::defaultStringLength(191);
+        Schema::create('cars', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('title');
+            $table->text('producer');
+            $table->integer('number_of_doors');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cars');
     }
 }
